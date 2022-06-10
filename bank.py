c8ad71f918607a7ff0ab3ea463e23a2c84260507
EPUULUSYAMEQWI <epuulucyamekwi@gmail.com>
2022-06-09 21:01:53 -0400

    first commit

--- bank.py
+++ bank.py
@@ -1,18 +1,37 @@
 
 class Account:
-    def __init__(self,name,number,balance,bankname) :
+    def __init__(self,name, account_number):
         self.name=name
-        self.number=number
-        self.balance=balance
-        self.bankname=bankname
-
+        self.number=account_number
+        self.balance = 0
+        self.deposits=[]
+        self.withdrawal=[]
+        
 
     def deposit(self,amount):
-        self.deposit=(input("You have deposited"))
-        self.balance+=amount
-        return f"Hello your current balance is {self.balance} "
+        
+       
 
-    def  withdraw(self,amount) :
-        self.deposit=(input("You have withdrawn"))
-        self.balance-=amount 
-        return f"Hello your current balance is {self.balance}"
+        if amount<=0:
+            return f"Deposit should be more than zero"
+        else:
+            self.balance += amount
+            self.deposits.append(amount)
+            return f"You have deposited {amount} your new balance is {self.balance}" ,self.deposits
+            
+    def withdraw(self,amount):
+        self.withdrawal -=amount
+        self.deposits.append(amount)
+
+        if amount>self.balance:
+            return f"Your balance is {self.balance},you cannot withdraw the {amount}"
+        elif amount <=0:
+            return f"Amount {amount} must be greater than zero" 
+
+        else:
+            self.balance-=amount
+            self.withdrawal.append(amount)
+            return f"You have withdrawn {amount} your balance is {self.balance}" ,self.withdrawal 
+
+    def deposit_statement(self):
+        print(*self.deposits,sep="\n")    
