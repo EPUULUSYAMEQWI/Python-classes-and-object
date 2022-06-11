6afce3aa6690aeb5cc97df2f3571dd45462f04a9
EPUULUSYAMEQWI <epuulucyamekwi@gmail.com>
2022-06-10 21:39:46 -0400

    commit secondchange

--- bank.py
+++ bank.py
@@ -3,6 +3,7 @@ class Account:
     def __init__(self,name, account_number):
         self.name=name
         self.number=account_number
+        self.transaction=100
         self.balance = 0
         self.deposits=[]
         self.withdrawal=[]
@@ -29,12 +30,27 @@ class Account:
             return f"Amount {amount} must be greater than zero" 
 
         else:
-            self.balance-=amount
+            self.balance-=amount+self.transaction
             self.withdrawal.append(amount)
-            return f"You have withdrawn {amount} your balance is {self.balance}" ,self.withdrawal 
+            return f"You have withdrawn {amount} your balance is {self.balance}" ,self.withdrawal
 
-    def deposit_statement(self):
-        print(*self.deposits,sep="\n") 
 
-    def withdraw_statement(self):
-        print(*self.withdrawal,sep="\n")
+        def deposit_statement(self):
+         for x in self.deposits:
+             print(f"Hello you have deposited {x} ",end="\n")
+
+
+
+        def withdrawal_statement(self):
+         for x in self.withdrawals:
+             print(f"Hello you have withdrawn {x} ",end="\n")
+
+
+
+
+
+    def current_balance(self):
+        return f"Hello your current balance is  {self.balance} "
+    
+
+    
